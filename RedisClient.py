085ca826e5e188eac11e64eccc7fe701bd79481a
Arnau Llop Iglesias <arnau.llop@estudiants.urv.cat>
2022-04-09 11:57:45 +0200

    Further developping redis pubsub indirect communication idea. Pending: 	Client structure 	Workers: 		Listening loop 		Pandas' implementation 		Workers' feedback to client

--- RedisClient.py
+++ RedisClient.py
@@ -3,4 +3,8 @@ import redis
 redis_host = 'localhost'
 redis_port = 6379
 
-workerList = {"a","b","c","d"}
+master = redis.StrictRedis(host=redis_host, port=redis_port, decode_responses=True)
+
+if __name__ == '__main__':
+    #To be implemented yet
+    exit        # INSTRUCTION TO BE DELETED!!! Only to avoid warnings due to unimplemented code
--- RedisSlave.py
+++ RedisSlave.py
@@ -5,30 +5,24 @@ redis_port = 6379
 
 master = redis.StrictRedis(host=redis_host, port=redis_port, decode_responses=True)
 
-def slave(name):
-    addSlave(name)
-    print("Number of workers: " + str(master.get("Workers")))
-    print("\nThe list of workers is:\n")
-    for i in master.get("Workers"):
-        print(str(master.lindex("LlistaWorkers", i))+"\n")
-    while True:
-        
-        break
+def slave():
+    registerNewSlave()
+    subscribeToMaster()
+#    listenToBoard()
 
-def addSlave(name):
-    master.rpush("LlistaWorkers", name)
-    workers = master.get("Workers")
-    if workers == "None":
-        numWorkers = 0
-    else:
-        numWorkers = int(workers)
-        
-    # Longitud de la llista, canviar.
-    master.set("Workers", numWorkers+1)
+def registerNewSlave():
+    workers=int(master.get("Workers"))      # Gets the number of currently registered slaves on the system. At least 0
+    master.set("Workers", workers+1)        # Increments the number of slaves on the server to increase the information recieved by the client
+
+def subscribeToMaster():
+    pubsub = master.pubsub()                # Generates the instance pubsub tied to the master server
+    pubsub.subscribe("Board")               # Applies pubsub to the Board we want to subscribe to
+
+#def listenToBoard():
 
 
 if __name__ == '__main__':
     master.set("Workers", 0)
-    slave("Slave0")
-    slave("Slave1")
-    slave("Slave2")
+    slave()
+#    slave()
+#    slave()
