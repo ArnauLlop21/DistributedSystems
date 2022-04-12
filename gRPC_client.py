7926a17c6d6c2fba3c504d2c7b6e7ee8d2a659c3
Àngel Gascón <angel.gascon@estudiants.urv.cat>
2022-04-12 17:34:37 +0200

    firts changes gRPC

--- gRPC_client.py
+++ gRPC_client.py
@@ -1,1 +1,18 @@
-print("new branch")
+import grpc
+
+# import the generated classes (client/server/master)
+
+
+# open a gRPC channel
+channel = grpc.insecure_channel('localhost:9000')
+
+# create a stub (client), proxy
+stub = generatedClass.InsultAPIStub(channel)
+
+response = stub.getInsults(InsultAPI_pb2.InsultListMessage())
+response2 = stub.insultme(InsultAPI_pb2.Request())
+response3 = stub.addInsult(InsultAPI_pb2.Request(requestName='cacota'))
+# et voilà
+print(response)
+print(response2)
+print(response3)
