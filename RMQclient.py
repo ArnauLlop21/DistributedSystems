6822624cadd85a93c15792d076614331416b13c7
Arnau Llop Iglesias <arnau.llop@estudiants.urv.cat>
2022-04-13 19:24:15 +0200

    RabbitMQ indirect communication finished Some issues to be solved. Known issues: 	Groupby is not working correctly

--- RMQclient.py
+++ RMQclient.py
@@ -1,4 +1,4 @@
-# RMQ server -> 0.0.0.0:5672  || localhost:5672 
+# RabbitMQ approach to server-client publish subscribe with reply request
 
 from email.base64mime import decodestring
 import pika as pk
@@ -7,24 +7,31 @@ import uuid
 import pandas as pd
 import time
 
+# Global configuration information
 rmqHost='localhost'
 rmqPort=5672
 exchBoard="pubsub"
 replyQueue="replyqueue"
+
+# Global variables 
 replyCache=[]
 timeCache=[]
 
+# Petition message.
+# Special format required:
+# str(fileToBeRead;operationToBeApplied;paramNeeded)
+# If no parameter is needed leave the third field as "null"
+# message = "titanic.csv;head;5"
+# message = "titanic.csv;columns;null"
+message = "titanic.csv;columns;null"
+
+# Connection configuration
 connection_parameters = pk.ConnectionParameters(rmqHost,rmqPort)
 connection = pk.BlockingConnection(connection_parameters)
 channel = connection.channel()
-
 reply_queue=channel.queue_declare(queue=replyQueue, exclusive=True)
-
-channel.exchange_declare(exchange=exchBoard, exchange_type=ExchangeType.fanout)
-
-message = "titanic.csv;max;Age"
-cor_id = str(uuid.uuid4())
-
+channel.exchange_declare(exchange=exchBoard, exchange_type=ExchangeType.fanout) # Creates the fanout (broadcast) exchange
+cor_id = str(uuid.uuid4())                                                      # Formality, not really used
 channel.basic_publish(
     exchange=exchBoard, 
     routing_key='', 
@@ -33,32 +40,35 @@ channel.basic_publish(
         correlation_id=cor_id,
         ),
     body=message)
-print(f"Message sent: {message}")
-live = True
-timeOut=100
+
+#print(f"Message sent: {message}")
+
+# Variables in charge of controlling the while loop
+timeOut=100     # Arbitrary maximum time. Set so the client wont be waiting more than 100sec for an answer
 timeAct=0
 timeIn=time.time()
-while (live == True and timeOut > timeAct):
 
+# This loop goes through until the maximum permitted time is exceeded
+while (timeOut >= timeAct):
+    # This line retrieves the information inside the reply_queue if existing.
+    # Gets "None" otherwise
     method, properties, body = channel.basic_get(queue=reply_queue.method.queue, auto_ack=True)
     timeAct=time.time()-timeIn
     if body :
-        answer=body.decode()
-        answer=list(answer.split(";;;;"))
-        replyCache.append(answer[0])
-        timeCache.append(float(answer[1]))
+        answer=body.decode()                    # Decodes the byte answer into a string.
+        answer=list(answer.split(";;;;"))       # Splits the string and creates and array with it
+        replyCache.append(answer[0])            # Saves the replies for further management
+        timeCache.append(float(answer[1]))      # Saves the times on the replies for further management
         timeMax=max(timeCache)
-        #print (timeMax)
-        timeOut=5*timeMax
-        #print (timeOut)
+        timeOut=5*timeMax                       # The maximum time to be waited is five times the maximum waiting time of the slaves
 
 if list(message.split(";"))[1] == "max" or list(message.split(";"))[1] == "min":
     for i in range(0, len(replyCache)):
         replyCache[i] = float(replyCache[i])
     if list(message.split(";"))[1] == "max":
-        print(max(replyCache))
+        print(max(replyCache))                  # If the answer is purely numeric then print the applied operation of all the answers
     else:
         print(min(replyCache))
 else:
-    print(replyCache)
+    print(replyCache)                           # Else then print all the answers
 
--- RMQworker.py
+++ RMQworker.py
@@ -1,15 +1,10 @@
-# RMQ server -> 0.0.0.0:5672  || localhost:5672 
-# Packages needed: PIKA
-# Use this code only as a tool to get further comprehension about rabbitMQ
-# This code is not, by anny matter, my property. 
-# Its been taken, with educational purposes, from the following video.
-# https://www.youtube.com/watch?v=Hy0cjAcxn_0&list=PLalrWAGybpB-UHbRDhFsBgXJM1g6T4IvO&index=12
-
-# This version uses the pubsub scheme.
+# RabbitMQ approach to server-client publish subscribe with reply request
 
 import pika as pk
 import time
 from pika.exchange_type import ExchangeType
+import pandas as pd
+import RMQOptions as ro
 
 rmqHost='localhost'
 rmqPort=5672
@@ -17,12 +12,45 @@ exchBoard="pubsub"
 
 def on_request_message_recieve(ch, method, properties, body):
     print("Recieved new message:",body, properties.correlation_id)
-    startTime=time.time()
-    time.sleep(0.5)
-    totalTime=str(time.time()-startTime)
-    reply = "10;;;;"+totalTime
+    startTime=time.time() # Starting the chrono
+    petition=body.decode()
+    petTpl=tuple(map(str, petition.split(";"))) # String into String tuple in order to manage information in an easier way
+    toBeReported=str(workInPanditas(petTpl))    # Calculates the value to be returned. Strings it
+    totalTime=str(time.time()-startTime)        
+    reply = toBeReported+";;;;"+totalTime       # String to be reported back. It includes the message itself, a separator token and the time taken to calculate.
     ch.basic_publish('',routing_key=properties.reply_to, body=reply)
     print("Finished processing")
+
+def workInPanditas(tuple):
+    df=pd.read_csv(tuple[0])
+    result=calculateSlave(df,tuple[1],str(tuple[2]))
+    return result
+
+def calculateSlave(df,option,parameter):    # Pandas' methods
+    if parameter == "null" :
+        #print("parameter null")
+        if option == "columns":
+            return ro.columns(df)
+        elif option == "items":
+            return ro.items(df)
+        else: return "Something went wrong"
+    else:
+        #print("parameter not null")
+        if option == "apply":
+            return ro.apply(df,parameter)
+        elif option == "groupby":
+            return ro.groupby(df,parameter)
+        elif option == "head":
+            return ro.head(df, int(parameter))
+        elif option == "isin":
+            return ro.isin(df,parameter)
+        elif option == "max":
+            return ro.max(df,parameter)
+        elif option == "min":
+            return ro.min(df,parameter)
+        else: return "Something went wrong"
+
+
 
 connection_parameters = pk.ConnectionParameters(rmqHost,rmqPort)
 connection = pk.BlockingConnection(connection_parameters)
@@ -34,15 +62,14 @@ channel = connection.channel()
 channel.basic_qos(prefetch_count=1)
 
 channel.exchange_declare(exchange=exchBoard, exchange_type=ExchangeType.fanout)
-
-queue = channel.queue_declare(queue='', exclusive=True)
-
-channel.queue_bind(exchange=exchBoard, queue=queue.method.queue)
-
-# channel.basic_consume(queue=queueBoard, auto_ack=True, on_message_callback=on_message_recieve)
-# We must remove the auto_ack=True parameter as we need to ack manually when the task is finished
-channel.basic_consume(queue=queue.method.queue, auto_ack=True, on_message_callback=on_request_message_recieve)
+# The "empty" queue field forces the server to create a "random" instance of a unique queue
+# This queue will be deleted as soon as it turns unused
+queueSlave = channel.queue_declare(queue='', exclusive=True)
+# Binding the specific queue created above to the broadcast exchange channel provided by the server
+channel.queue_bind(exchange=exchBoard, queue=queueSlave.method.queue)
+# Configuration for consuming the queue messages
+channel.basic_consume(queue=queueSlave.method.queue, auto_ack=True, on_message_callback=on_request_message_recieve)
 
 print("Started consuming messages")
-
+# Endless loop. Fetches the messages inside the channelSlave
 channel.start_consuming()
