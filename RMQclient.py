c4bc0cf4e9af44ede91c24bdd8c2dcedf4b8be40
Arnau Llop Iglesias <arnau.llop@estudiants.urv.cat>
2022-04-13 13:12:51 +0200

    RabbitMQ third learning approach. WE DO NOT OWN THIS CODE code extracted from JUMPSTARTCS youtube channel
    RabbitMQ scheme used in this example:
    	publish subscribe. Broadcast.

--- RMQclient.py
+++ RMQclient.py
@@ -1,37 +1,35 @@
 # RMQ server -> 0.0.0.0:5672  || localhost:5672 
 # Packages needed: PIKA
-# This version does not implement publish subscribe scheme. 
 # Use this code only as a tool to get further comprehension about rabbitMQ
 # This code is not, by anny matter, my property. 
 # Its been taken, with educational purposes, from the following video.
-# https://www.youtube.com/watch?v=hi8DjlcbN4A&list=PLalrWAGybpB-UHbRDhFsBgXJM1g6T4IvO&index=8
-# https://www.youtube.com/watch?v=jXBd0jP6EoE&list=PLalrWAGybpB-UHbRDhFsBgXJM1g6T4IvO&index=9
+# https://www.youtube.com/watch?v=Hy0cjAcxn_0&list=PLalrWAGybpB-UHbRDhFsBgXJM1g6T4IvO&index=12
 
-# This version uses the competing worker scheme. NOT TO BE USED in the assignment.
-# Educational purposes
+# This version uses the pubsub scheme.
 
 import pika as pk
 import random as rd
 import time
+from pika.exchange_type import ExchangeType
+
 rmqHost='localhost'
 rmqPort=5672
 queueBoard='letterbox'
+exchBoard="pubsub"
 
 connection_parameters = pk.ConnectionParameters(rmqHost,rmqPort)
 connection = pk.BlockingConnection(connection_parameters)
 channel = connection.channel()
 
-channel.queue_declare(queue=queueBoard)
+channel.exchange_declare(exchange=exchBoard, exchange_type=ExchangeType.fanout)
 
 # Now that we have our queue set up we can publish a message.
 # IMPORTANT NOTE!
 # We don't ever publish a message directly to a queue, we must use the exchange.
 # But to simplify we are going to use the default exchange
 
-petID=0
-while (True):
-    message = f"Petition number: {petID}"
-    channel.basic_publish(exchange='', routing_key=queueBoard, body=message)
-    print("Message sent")
-    petID += 1
-    time.sleep(rd.randint(1,3))
+message = f"Message to be broadcasted"
+channel.basic_publish(exchange=exchBoard, routing_key='', body=message)
+print(f"Message sent: {message}")
+connection.close()
+
--- RMQworker.py
+++ RMQworker.py
@@ -1,25 +1,22 @@
 # RMQ server -> 0.0.0.0:5672  || localhost:5672 
 # Packages needed: PIKA
-# This version does not implement publish subscribe scheme. 
 # Use this code only as a tool to get further comprehension about rabbitMQ
 # This code is not, by anny matter, my property. 
 # Its been taken, with educational purposes, from the following video.
-# https://www.youtube.com/watch?v=hi8DjlcbN4A&list=PLalrWAGybpB-UHbRDhFsBgXJM1g6T4IvO&index=8
-# https://www.youtube.com/watch?v=jXBd0jP6EoE&list=PLalrWAGybpB-UHbRDhFsBgXJM1g6T4IvO&index=9
+# https://www.youtube.com/watch?v=Hy0cjAcxn_0&list=PLalrWAGybpB-UHbRDhFsBgXJM1g6T4IvO&index=12
 
-# This version uses the competing worker scheme. NOT TO BE USED in the assignment.
-# Educational purposes
+# This version uses the pubsub scheme.
 
 import pika as pk
 import time
+from pika.exchange_type import ExchangeType
 
 rmqHost='localhost'
 rmqPort=5672
-queueBoard='letterbox'
+exchBoard="pubsub"
 
 def on_message_recieve(ch, method, properties, body):
     print(f"Recieved new message: {body}")
-    time.sleep(4) # This is to simulate a task being processed for the sake of this example
     ch.basic_ack(delivery_tag=method.delivery_tag) # Manual ack
     print("Finished processing")
 
@@ -27,17 +24,20 @@ connection_parameters = pk.ConnectionParameters(rmqHost,rmqPort)
 connection = pk.BlockingConnection(connection_parameters)
 channel = connection.channel()
 
-# It is mostly okay to declare the same queue more than once 
-# as rabbitmq will only execute the first instance of it.
-channel.queue_declare(queue=queueBoard)
 
 # IMPORTANT
 # This means that a worker will only process a petition at a time
 channel.basic_qos(prefetch_count=1)
 
+channel.exchange_declare(exchange=exchBoard, exchange_type=ExchangeType.fanout)
+
+queue = channel.queue_declare(queue='', exclusive=True)
+
+channel.queue_bind(exchange=exchBoard, queue=queue.method.queue)
+
 # channel.basic_consume(queue=queueBoard, auto_ack=True, on_message_callback=on_message_recieve)
 # We must remove the auto_ack=True parameter as we need to ack manually when the task is finished
-channel.basic_consume(queue=queueBoard, on_message_callback=on_message_recieve)
+channel.basic_consume(queue=queue.method.queue, on_message_callback=on_message_recieve)
 
 print("Started consuming messages")
 
